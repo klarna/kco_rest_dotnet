cc02d8fae22b5b3c31a24322176823ef1e4dcebe
Aleksander Zinovyev <alexions@yandex.ru>
2018-10-16 15:30:47 +0200

    Add testing prototype structure. Tests: Add tests for Exceptions.

---
+++ src/test/com/klarna/rest/ProtocolExceptionTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 Klarna AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klarna.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Test cases for the ProtocolException class.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProtocolExceptionTest extends TestCase {
+
+    @Test
+    public void testUnexpectedStatus() {
+        ProtocolException ex = ProtocolException.unexpectedStatus(404);
+
+        assertEquals("Unexpected response status code: 404", ex.getMessage());
+    }
+}
