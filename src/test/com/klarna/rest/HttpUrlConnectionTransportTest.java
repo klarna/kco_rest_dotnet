f0b967967e4dfd04efa011158d8046e86db231e3
Aleksander Zinovyev <alexions@yandex.ru>
2018-10-17 14:44:08 +0200

    Tests: Cover HttpUrlConnection with tests.

--- src/test/com/klarna/rest/HttpUrlConnectionTransportTest.java
+++ src/test/com/klarna/rest/HttpUrlConnectionTransportTest.java
@@ -16,16 +16,28 @@
 
 package com.klarna.rest;
 
+import edu.emory.mathcs.backport.java.util.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.util.Map;
+import java.util.*;
+
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HttpUrlConnectionTransportTest extends TestCase {
@@ -38,12 +50,164 @@ public class HttpUrlConnectionTransportTest extends TestCase {
 
     @Test
     public void testBuildConnection() throws IOException {
-        HttpURLConnection conn = transport.testBuildConnection("/orders", null);
-        assertEquals(new URL("https://api.playground.klarna.com/orders"), conn.getURL());
+        HashMap<String, String> testHeaders = new HashMap<>();
+        testHeaders.put("Test", "123");
+        HttpURLConnection conn = transport.testBuildConnection("/orders", testHeaders);
+
+        assertEquals(new URL(Transport.EU_TEST_BASE_URL + "/orders"), conn.getURL());
+        assertEquals(transport.getUserAgent(), conn.getRequestProperty("User-Agent"));
+        assertEquals(MediaType.APPLICATION_JSON, conn.getRequestProperty("Content-Type"));
+        assertEquals("123", conn.getRequestProperty("Test"));
+        assertEquals(transport.getTimeout(), conn.getConnectTimeout());
+        assertEquals(transport.getTimeout(), conn.getReadTimeout());
+    }
+
+    @Test
+    public void testGetRequest() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+
+        ApiResponse response = transport.get("/test-url", null);
+        response.validator()
+                .expectStatusCode(Response.Status.OK)
+                .expectContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test(expected = ApiException.class)
+    public void testUnsuccessfullRequest() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(500);
+
+        ApiResponse response = transport.get("/test-url", null);
+        response.validator();
+    }
+
+    @Test(expected = ProtocolException.class)
+    public void testUnexpectedCode() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(201);
+
+        ApiResponse response = transport.get("/test-url", null);
+        response.validator().expectStatusCode(Response.Status.NO_CONTENT);
+    }
+
+    @Test(expected = ContentTypeException.class)
+    public void testUnexpectedContentType() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_OCTET_STREAM);
+                }
+            });
+        }});
+
+        ApiResponse response = transport.get("/test-url", null);
+        response.validator()
+                .expectStatusCode(Response.Status.OK)
+                .expectContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Test
+    public void testUserAgentProperty() {
+        transport.setUserAgent("No-Agent");
+        assertEquals("No-Agent", transport.getUserAgent());
+    }
+
+    @Test
+    public void testTimeoutPropery() {
+        transport.setTimeout(1);
+        assertEquals(1, transport.getTimeout());
+    }
+
+    @Test
+    public void testTransportTimeout() {
+        HttpUrlConnectionTransport t = new HttpUrlConnectionTransport("0","sharedSecret", Transport.EU_TEST_BASE_URL);
+        t.setTimeout(1);
+        try {
+            ApiResponse response = t.get("/", null);
+        } catch (IOException e) {
+            return;
+        }
+        fail("No timeout exception");
+    }
+
+    @Test
+    public void testWrongEndpoint() throws IOException {
+        HttpUrlConnectionTransport t = new HttpUrlConnectionTransport("0","sharedSecret", Transport.EU_TEST_BASE_URL);
+        ApiResponse response = t.get("/wrong-service-endpoint", null);
+        try {
+            response.validator();
+        } catch (ApiException e) {
+            assertEquals(404, e.getHttpStatus());
+            return;
+        }
+        fail("Validator did not throw exception");
+    }
+
+    @Test
+    public void testBodyResponse() throws IOException {
+        final String payload = "{\"hello\": 123}";
+
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(payload.getBytes());
+        InputStream is = new ByteArrayInputStream(out.toByteArray());
+        when(transport.conn.getInputStream()).thenReturn(is);
+
+        ApiResponse response = transport.get("/hello", null);
+        assertEquals(payload, new String(response.getBody()));
+    }
+
+    @Test
+    public void testErrorResponse() throws IOException {
+        final String payload = "{ \"error_code\" : \"CODE_123\", " +
+                "\"error_messages\" : [\"Missing required field\"], " +
+                "\"correlation_id\" : \"12345-asdf\", " +
+                "\"service_version\": \"0x8903\" }";
+
+        when(transport.conn.getResponseCode()).thenReturn(400);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(payload.getBytes());
+        InputStream is = new ByteArrayInputStream(out.toByteArray());
+        when(transport.conn.getErrorStream()).thenReturn(is);
+
+        ApiResponse response = transport.get("/error-message", null);
+        try {
+            response.validator();
+        } catch (ApiException e) {
+            assertEquals(400, e.getHttpStatus());
+            assertEquals("12345-asdf", e.getErrorMessage().getCorrelationId());
+            assertEquals("CODE_123", e.getErrorMessage().getErrorCode());
+            assertEquals(new ArrayList<String>(){{add("Missing required field");}}, e.getErrorMessage().getErrorMessages());
+            assertEquals("0x8903", e.getErrorMessage().getServiceVersion());
+            return;
+        }
+        fail("Unable to parse error message");
     }
 }
 
 class FakeHttpUrlConnectionTransport extends HttpUrlConnectionTransport {
+    @Mock public HttpURLConnection conn = mock(HttpURLConnection.class);
+
     FakeHttpUrlConnectionTransport(String merchantId, String sharedSecret, URI uri) {
         super(merchantId, sharedSecret, uri);
     }
@@ -58,7 +222,7 @@ class FakeHttpUrlConnectionTransport extends HttpUrlConnectionTransport {
 
     @Override
     protected HttpURLConnection buildConnection(String path, Map<String, String> headers) throws IOException {
-        HttpURLConnection conn = null;
+        doNothing().when(conn).setRequestMethod(isA(String.class));
         return conn;
     }
 }
