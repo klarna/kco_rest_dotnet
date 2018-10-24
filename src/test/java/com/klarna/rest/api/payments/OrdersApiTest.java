6c8ef92ac2be5cf9a0c0c4907ffe403ef383a07e
Aleksander Zinovyev <alexions@yandex.ru>
2018-10-24 17:22:33 +0200

    Tests: Cover Payments API.

---
+++ src/test/java/com/klarna/rest/api/payments/OrdersApiTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 Klarna AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klarna.rest.api.payments;
+
+import com.klarna.rest.FakeHttpUrlConnectionTransport;
+import com.klarna.rest.TestCase;
+import com.klarna.rest.model.payments.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrdersApiTest extends TestCase {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    private FakeHttpUrlConnectionTransport transport;
+
+    @Before
+    public void setUp() {
+        transport = new FakeHttpUrlConnectionTransport();
+    }
+
+    @Test
+    public void testCreate() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        final String payload = "{ \"order_id\": \"0b1d9815\", \"redirect_url\": \"https://credit.klarna.com/v1/sessions/0b1d9815-165e-42e2-8867-35bc03789e00/redirect\", \"fraud_status\": \"ACCEPTED\" }";
+        when(transport.conn.getInputStream()).thenReturn(this.makeInputStream(payload));
+
+        CreateOrderRequest data = new CreateOrderRequest(){
+            {
+                setOrderAmount(100L);
+                setAutoCapture(false);
+                setMerchantUrls(new MerchantUrls(){
+                    {
+                        setConfirmation("https://example.com/confirm");
+                    }
+                });
+            }
+        };
+
+        OrdersApi api = new OrdersApi(transport);
+        Order order = api.create("auth-token", data);
+
+        verify(transport.conn, times(1)).setRequestMethod("POST");
+        assertEquals("/payments/v1/authorizations/auth-token/order", transport.requestPath);
+        assertEquals("0b1d9815", order.getOrderId());
+        assertEquals("ACCEPTED", order.getFraudStatus());
+
+        final String requestPayout = transport.requestPayout.toString();
+        assertTrue(requestPayout.contains("\"order_amount\":100"));
+        assertTrue(requestPayout.contains("\"confirmation\":\"https://example.com/confirm\""));
+    }
+
+    @Test
+    public void testGenerateToken() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        final String payload = "{ \"token_id\": \"0b1d9815\", \"redirect_url\": \"https://credit.klarna.com/v1/sessions/0b1d9815-165e-42e2-8867-35bc03789e00/redirect\" }";
+        when(transport.conn.getInputStream()).thenReturn(this.makeInputStream(payload));
+
+        CustomerTokenCreationRequest data = new CustomerTokenCreationRequest(){
+            {
+                setIntendedUse(IntendedUseEnum.SUBSCRIPTION);
+                setPurchaseCountry("US");
+            }
+        };
+
+        OrdersApi api = new OrdersApi(transport);
+        CustomerTokenCreationResponse token = api.generateToken("auth-token", data);
+
+        verify(transport.conn, times(1)).setRequestMethod("POST");
+        assertEquals("/payments/v1/authorizations/auth-token/customer-token", transport.requestPath);
+        assertEquals("0b1d9815", token.getTokenId());
+        assertEquals("https://credit.klarna.com/v1/sessions/0b1d9815-165e-42e2-8867-35bc03789e00/redirect", token.getRedirectUrl());
+
+        final String requestPayout = transport.requestPayout.toString();
+        assertTrue(requestPayout.contains("\"intended_use\":\"SUBSCRIPTION\""));
+        assertTrue(requestPayout.contains("\"purchase_country\":\"US\""));
+        assertFalse(requestPayout.contains("billing_address"));
+    }
+
+    @Test
+    public void testAcknowledgeOrder() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(204);
+
+        OrdersApi api = new OrdersApi(transport);
+        api.cancelAuthorization("auth-token");
+
+        verify(transport.conn, times(1)).setRequestMethod("DELETE");
+        assertEquals("/payments/v1/authorizations/auth-token", transport.requestPath);
+    }
+}
---
+++ src/test/java/com/klarna/rest/api/payments/SessionsApiTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2018 Klarna AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.klarna.rest.api.payments;
+
+import com.klarna.rest.FakeHttpUrlConnectionTransport;
+import com.klarna.rest.TestCase;
+import com.klarna.rest.model.payments.*;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SessionsApiTest extends TestCase {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    private FakeHttpUrlConnectionTransport transport;
+
+    @Before
+    public void setUp() {
+        transport = new FakeHttpUrlConnectionTransport();
+    }
+
+    @Test
+    public void testCreate() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        final String payload = "{ \"session_id\": \"0b1d9815-165e-42e2\", \"client_token\": \"eyJhb.ewogI\", \"payment_method_categories\": [ { \"identifier\": \"pay_later\", \"name\": \"Pay Later\" } ] }";
+        when(transport.conn.getInputStream()).thenReturn(this.makeInputStream(payload));
+
+        Session data = new Session(){
+            {
+                setOrderAmount(100L);
+                setMerchantUrls(new MerchantUrls(){
+                    {
+                        setConfirmation("https://example.com/confirm");
+                    }
+                });
+            }
+        };
+
+        SessionsApi api = new SessionsApi(transport);
+        MerchantSession session = api.create(data);
+
+        verify(transport.conn, times(1)).setRequestMethod("POST");
+        assertEquals("/payments/v1/sessions", transport.requestPath);
+        assertEquals("0b1d9815-165e-42e2", session.getSessionId());
+        assertEquals("eyJhb.ewogI", session.getClientToken());
+        assertEquals("pay_later", session.getPaymentMethodCategories().get(0).getIdentifier());
+
+        final String requestPayout = transport.requestPayout.toString();
+        assertTrue(requestPayout.contains("\"order_amount\":100"));
+        assertTrue(requestPayout.contains("\"confirmation\":\"https://example.com/confirm\""));
+    }
+
+    @Test
+    public void testFetch() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(200);
+        when(transport.conn.getHeaderFields()).thenReturn(new HashMap<String, List<String>>(){{
+            put("Content-Type", new ArrayList<String>(){
+                {
+                    add(MediaType.APPLICATION_JSON);
+                }
+            });
+        }});
+        final String payload = "{ \"purchase_country\": \"US\", \"order_amount\": 100, \"status\": \"incomplete\", \"client_token\": \"eyJhbGciOi.ewogIC\" }";
+        when(transport.conn.getInputStream()).thenReturn(this.makeInputStream(payload));
+
+        SessionsApi api = new SessionsApi(transport);
+        Session session = api.fetch("my-session-id");
+
+        verify(transport.conn, times(1)).setRequestMethod("GET");
+        assertEquals("/payments/v1/sessions/my-session-id", transport.requestPath);
+        assertEquals("US", session.getPurchaseCountry());
+        Long amount = 100L;
+        assertEquals(amount, session.getOrderAmount());
+        assertEquals(Session.StatusEnum.INCOMPLETE, session.getStatus());
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        when(transport.conn.getResponseCode()).thenReturn(204);
+
+        Session data = new Session(){
+            {
+                setOrderAmount(100L);
+                setMerchantUrls(new MerchantUrls(){
+                    {
+                        setConfirmation("https://example.com/confirm");
+                    }
+                });
+            }
+        };
+
+        SessionsApi api = new SessionsApi(transport);
+        api.update("my-session-id", data);
+
+        verify(transport.conn, times(1)).setRequestMethod("POST");
+        assertEquals("/payments/v1/sessions/my-session-id", transport.requestPath);
+
+        final String requestPayout = transport.requestPayout.toString();
+        assertTrue(requestPayout.contains("\"order_amount\":100"));
+        assertTrue(requestPayout.contains("\"confirmation\":\"https://example.com/confirm\""));
+    }
+}
