09de200d1b49ab2d8cc4f377323caa0b86a2edce
Mattias Nording <mattias.nording@klarna.com>
2019-01-31 11:23:46 +0100

    Fixes issue  #15 adding all token details

---
+++ Klarna.Rest/Klarna.Rest.Core/Model/CustomerDirectDebitTokenDetails.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+namespace Klarna.Rest.Core.Model
+{
+    public class CustomerDirectDebitTokenDetails
+    {
+        [JsonProperty(PropertyName = "brand")]
+        public string Brand;
+        [JsonProperty(PropertyName = "masked_number")]
+        public string MaskedNumber;
+    }
+}
---
+++ Klarna.Rest/Klarna.Rest.Core/Model/CustomerTokenCardDetails.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+namespace Klarna.Rest.Core.Model
+{
+    public class CustomerTokenCardDetails
+    {
+        [JsonProperty(PropertyName = "brand")]
+        public string Brand;
+        [JsonProperty(PropertyName = "masked_number")]
+        public string MaskedNumber;
+        [JsonProperty(PropertyName = "expiry_date")]
+        public string ExpiryDate;
+    }
+}
