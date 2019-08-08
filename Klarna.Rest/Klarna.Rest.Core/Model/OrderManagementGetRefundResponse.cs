e65b0f7b70327aec20ef09bf05ecd5da94a8b90d
Alexander Zinovev <alexions@yandex.ru>
2019-08-08 15:15:10 +0200

    Fix #57: Combine similar OrderManager Refund models into a single one.  Add missing field to OrderManagementRefund model.

--- Klarna.Rest/Klarna.Rest.Core/Model/OrderManagementGetRefundResponse.cs
+++ Klarna.Rest/Klarna.Rest.Core/Model/OrderManagementGetRefundResponse.cs
@@ -3,48 +3,7 @@ using Newtonsoft.Json;
 
 namespace Klarna.Rest.Core.Model
 {
-    public class OrderManagementGetRefundResponse
+    public class OrderManagementGetRefundResponse : OrderManagementRefund
     {
-        /// <summary>
-        /// Refund ID
-        /// </summary>
-        [JsonProperty(PropertyName = "refund_id")]
-        public string RefundId { get; set; }
-
-        /// <summary>
-        /// Refunded amount in minor units.
-        /// </summary>
-        [JsonProperty(PropertyName = "refunded_amount")]
-        public int RefundedAmount { get; set; }
-        
-        /// <summary>
-        /// Date for refund
-        /// </summary>
-        [JsonProperty(PropertyName = "refunded_at")]
-        public string RefundedAt { get; set; }
-        
-        /// <summary>
-        /// Description of the refund shown to the customer. Max length is 255 characters.
-        /// </summary>
-        [JsonProperty(PropertyName = "description")]
-        public string Description { get; set; }
-        
-        /// <summary>
-        /// Order lines for the refund shown to the customer. Optional but increases the customer experience. Maximum 1000 order lines.
-        /// </summary>
-        [JsonProperty(PropertyName = "order_lines")]
-        public ICollection<OrderLine> OrderLines { get; set; }
-        
-        /// <summary>
-        /// Order lines for the refund shown to the customer. Optional but increases the customer experience. Maximum 1000 order lines.
-        /// </summary>
-        [JsonProperty(PropertyName = "processed_order_lines")]
-        public ICollection<OrderLine> ProcessedOrderLines { get; set; }
-        
-        /// <summary>
-        /// 
-        /// </summary>
-        [JsonProperty(PropertyName = "merchant_reversal")]
-        public bool MerchantReversal { get; set; }
     }
 }
