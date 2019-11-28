64417be60508811591f7c0e8e0c021ac89797c2f
Alexander Zinovev <alexions@yandex.ru>
2019-11-28 17:13:25 +0100

    Tests: Add backward compatibility checks for newly created models.

--- Klarna.Rest/Klarna.Rest.Core.Tests/Models/ModelCompatibility.cs
+++ Klarna.Rest/Klarna.Rest.Core.Tests/Models/ModelCompatibility.cs
@@ -1,6 +1,6 @@
 using Klarna.Rest.Core.Model;
 using Klarna.Rest.Core.Model.HostedPaymentPage;
-
+using Klarna.Rest.Core.Model.OrderManagement;
 using Xunit;
 
 namespace Klarna.Rest.Core.Tests.Models
@@ -14,6 +14,24 @@ namespace Klarna.Rest.Core.Tests.Models
         public void HppSessionCreationCompatibility()
         {
             Assert.True(typeof(SessionResponseV1).IsSubclassOf(typeof(HostedPaymentPageSessionStatus)));
+        }
+        
+        [Fact]
+        public void OrderManagementCaptureCompatibility()
+        {
+            Assert.True(typeof(Capture).IsSubclassOf(typeof(OrderManagementCapture)));
+        }
+        
+        [Fact]
+        public void OrderManagementOrderCompatibility()
+        {
+            Assert.True(typeof(Order).IsSubclassOf(typeof(OrderManagementOrder)));
+        }
+        
+        [Fact]
+        public void OrderManagementRefundCompatibility()
+        {
+            Assert.True(typeof(Refund).IsSubclassOf(typeof(OrderManagementGetRefundResponse)));
         }
     }
 }
