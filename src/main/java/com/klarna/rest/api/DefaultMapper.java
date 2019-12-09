a1fbb820d612e2d2778a6901d9824536b104b851
Alexander Zinovev <alexions@yandex.ru>
2019-12-09 14:50:23 +0100

    Add com.fasterxml.jackson.datatype dependency to cover OffsetDateTime from string construction. Fix \#12

--- src/main/java/com/klarna/rest/api/DefaultMapper.java
+++ src/main/java/com/klarna/rest/api/DefaultMapper.java
@@ -19,6 +19,7 @@ package com.klarna.rest.api;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.text.SimpleDateFormat;
 
@@ -27,6 +28,7 @@ import java.text.SimpleDateFormat;
  */
 public class DefaultMapper extends ObjectMapper {
     public DefaultMapper() {
+        this.registerModule(new JavaTimeModule());
         this.findAndRegisterModules();
         this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
